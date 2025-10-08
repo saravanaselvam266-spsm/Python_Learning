35ed9405b72d9a46ba939684b7ba73e80d99f86c
saravanaselvam266-spsm <saravana.selvam266@gmail.com>
2025-10-08 14:38:44 +0530

    adding new python file

---
+++ main.py
@@ -0,0 +1,14 @@
+#Adding New Python code
+
+def eligibility_criteria(age):
+    #start
+    if age >=18 :
+        print("Yes")
+    else :
+        print("No")
+    #stop
+
+eligibility_criteria(28)
+eligibility_criteria(90)
+eligibility_criteria(17)
+eligibility_criteria(16)
