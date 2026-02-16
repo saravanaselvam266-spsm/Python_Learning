17624ddee4f50c379287ed4bc06030df2d9b4eb9
saravanaselvam266-spsm <saravana.selvam266@gmail.com>
2026-02-16 10:44:08 +0530

    for binary search another option added

--- Search_algoritham/binary_search.py
+++ Search_algoritham/binary_search.py
@@ -1,5 +1,7 @@
 # binary search algorithm 
 
+# option 1
+
 def binary_search(arr,x):
     low = 0 
     high = len(arr) - 1 
@@ -20,3 +22,24 @@ print(binary_search([1,2,3,4,5,6,7,8,9,10],6))
 print(binary_search([1,2,3,4,5,6,7,8,9,10],9))
 print(binary_search([1,2,3,4,5,6,7,8,9,10],4))
 print(binary_search([1,2,3,4,5,6,7,8,9,10],12))
+
+
+
+# Option 2
+
+def search_algorithm(arr,low,high,x):
+    if (high < low):
+        return False
+    mid = (low + high) // 2
+    if x > arr[mid] :
+        return search_algorithm(arr,mid+1,high,x)
+    elif x < arr[mid]:
+        return search_algorithm(arr,low,mid-1,x)
+    elif x == arr[mid]:
+        return True , arr[mid]
+    else :
+        return False
+    
+print(search_algorithm([1,2,3,4,5,6,7,8,9,10],0,9,3))
+
+    
